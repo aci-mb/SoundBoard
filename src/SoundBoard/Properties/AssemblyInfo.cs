f8d6958bd0e14c5e9085908d257059dd362b0441
Markus Bettig <markus.bettig@gmx.net>
2016-11-15 21:53:10 +0100

    Updated Assembly Info (Version 2.0.0.0)

--- src/SoundBoard/Properties/AssemblyInfo.cs
+++ src/SoundBoard/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AcillatemSoundBoard")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Acillatem SoundBoard")]
+[assembly: AssemblyDescription("Pen & Paper Soundboard WPF application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AcillatemSoundBoard")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Acillatem SoundBoard")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,6 +49,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: InternalsVisibleTo("AcillatemSoundBoard.Tests")]
